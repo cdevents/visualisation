eea0ff4d97f384962892626e819f1d1df656adb7
afenner <Andrew.fenner@est.tech>
2023-07-27 11:14:14 +0100

    Initial python based visualization POC
    Using prometheus/loki/promtail/grafana/arangodb/nodegraphapi
    
    Signed-off-by: afenner <Andrew.fenner@est.tech>
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

---
+++ python-poc/python-reciever/app.py
@@ -0,0 +1,83 @@
+import os
+
+from flask import Flask
+from flask import request
+from cloudevents.http import from_http
+import requests
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+from prometheus_client import Counter
+import json
+import logging
+import argparse
+import time
+import sys
+
+counters = {}
+
+def create_counter(json_data_str):
+    # Extract relevant fields from JSON data and write them to the log file
+    log_entry = f"CD_EVent: {json_data_str}"
+    logging.info(log_entry)
+    json_data = json.loads(json_data_str)
+    counter = None
+    metricname = json_data["context"]["type"].replace(".","_")
+    if metricname in counters:
+        counter = counters[metricname]
+    else:
+        if metricname == "dev.cdevents.change.merged.0.1.0".replace(".","_"):
+            counter = Counter(metricname, 'Counter for' + str(json_data["context"]["type"]),["source"])
+        elif metricname == "dev.cdevents.testsuite.finished.0.1.0".replace(".","_"):
+            counter = Counter(metricname, 'Counter for' + str(json_data["context"]["type"]),["source","outcome"])
+        else:
+            counter = Counter(metricname, 'Counter for' + str(json_data["context"]["type"]))
+        counters[metricname] = counter
+
+    if metricname == "dev.cdevents.change.merged.0.1.0".replace(".","_"):
+        # print("json_data[\"subject\"][\"content\"][\"repository\"][\"id\"]" + str(json_data["subject"]["content"]["repository"]["id"]))
+        counter.labels(source=json_data["subject"]["repository"]["id"]).inc()
+    elif metricname == "dev.cdevents.testsuite.finished.0.1.0".replace(".","_"):
+        # print("json_data[\"subject\"][\"content\"][\"repository\"][\"id\"]" + str(json_data["subject"]["content"]["repository"]["id"]))
+        counter.labels(source=json_data["subject"]["source"],outcome=json_data["subject"]["outcome"]).inc()
+
+    else:
+        counter.inc()
+
+
+
+app = Flask(__name__)
+
+# @app.route('/',methods = ['POST', 'GET'])
+
+#@app.route('/', defaults={'path': ''},methods = ['POST', 'GET'])
+@app.route('/',methods = ['POST', 'GET'])
+def hello_world():
+#def hello_world(path):
+    # create a CloudEvent
+    event = from_http(request.headers, request.get_data())
+
+    # you can access cloudevent fields as seen below
+    #print(
+    #    f" EVENT {event}"
+    #    f" request.get_data() {request.get_data()}"
+    #)
+    create_counter(request.get_data())
+    return "", 204
+    
+#    target = os.environ.get('TARGET', 'World')
+#    print ("request data" +str(request.data))
+#    print ("request data" +str(request))
+#    print ("request data" +str(path))
+#    return 'AFR Hello {} {} !\n'.format(target,path)
+
+if __name__ == "__main__":
+    #start_http_server(8000)
+    # time.sleep(60)
+    # Add prometheus wsgi middleware to route /metrics requests
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+        '/metrics': make_wsgi_app()
+    })
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+
