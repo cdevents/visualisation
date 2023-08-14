6950e4227f6fa8223076fa63a70e7284481c7204
rjalander <jalander.ramagiri@est.tech>
2023-08-14 16:17:12 +0000

    update proerties value

--- poc/cdevents-visualiser/src/main/java/dev/cdevents/visualiser/controller/MonitorCDEventsController.java
+++ poc/cdevents-visualiser/src/main/java/dev/cdevents/visualiser/controller/MonitorCDEventsController.java
@@ -25,6 +25,7 @@ public class MonitorCDEventsController {
 
     @RequestMapping(value = "/visualiser", method = RequestMethod.POST)
     public ResponseEntity<Void> receiveCDEvent(@RequestBody CloudEvent cdEvent){
+	    System.out.println("Received CDEvent " + cdEvent.getType());
         for (MonitorCDEvents monitorCDEvents : monitorImplementations) {
             monitorCDEvents.processCDEvent(cdEvent);
         }
