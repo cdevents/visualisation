93d21ec915a360f9eaa6dbb65c7a080d3237a169
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-10-12 10:28:45 +0100

    adding jackson dependency

--- poc/cdevents-visualiser/src/main/java/dev/cdevents/visualiser/controller/MonitorCDEventsController.java
+++ poc/cdevents-visualiser/src/main/java/dev/cdevents/visualiser/controller/MonitorCDEventsController.java
@@ -1,7 +1,11 @@
 package dev.cdevents.visualiser.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.cdevents.visualiser.service.MonitorCDEvents;
 import io.cloudevents.CloudEvent;
+import io.cloudevents.core.provider.EventFormatProvider;
+import io.cloudevents.jackson.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.FAIL_ON_EMPTY_BEANS;
 
 @RestController
 @RequestMapping(value = "/cdevents")
@@ -25,7 +31,16 @@ public class MonitorCDEventsController {
 
     @RequestMapping(value = "/visualiser", method = RequestMethod.POST)
     public ResponseEntity<Void> receiveCDEvent(@RequestBody CloudEvent cdEvent){
-        System.out.println("Received CDEvent " + cdEvent.getType());
+        ObjectMapper objectMapper = new ObjectMapper();
+        EventFormatProvider.getInstance().registerFormat(new JsonFormat());
+        objectMapper.registerModule(JsonFormat.getCloudEventJacksonModule());
+        objectMapper.disable(FAIL_ON_EMPTY_BEANS);
+
+        try {
+            System.out.println("Received CDEvent " + objectMapper.writeValueAsString(cdEvent));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         for (MonitorCDEvents monitorCDEvents : monitorImplementations) {
             monitorCDEvents.processCDEvent(cdEvent);
         }
